3a88528149d2e38f49697852aa0a9d0d85810f0c
BuildTools <rergne63200@gmail.com>
2024-02-01 20:49:54 +0100

    Initial commit

---
+++ src/main/java/net/peg/branlexcraft/BranlexCraftClient.java
@@ -0,0 +1,10 @@
+package net.peg.branlexcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BranlexCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/peg/branlexcraft/BranlexCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.peg.branlexcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BranlexCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
