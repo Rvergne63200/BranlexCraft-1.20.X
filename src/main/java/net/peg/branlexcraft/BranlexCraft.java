dd2e82b462c82d1fb82d2283ec105378d634d032
BuildTools <rergne63200@gmail.com>
2024-02-03 02:23:26 +0100

    Add branlex items

--- src/main/java/net/peg/branlexcraft/BranlexCraft.java
+++ src/main/java/net/peg/branlexcraft/BranlexCraft.java
@@ -2,6 +2,8 @@ package net.peg.branlexcraft;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.peg.branlexcraft.item.ModItemGroups;
+import net.peg.branlexcraft.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,8 @@ public class BranlexCraft implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+
 	}
 }
